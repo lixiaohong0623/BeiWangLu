d27fc1086808bc032e1d361165c6213ae1bdee82
lixiaohongIM <435950684@qq.com>
2015-12-24 11:30:22 +0800

    更改主题、添加编辑界面

--- app/src/main/java/com/example/lixiaohong/beiwanglu/Activity/AddNewNotificationActivity.java
+++ app/src/main/java/com/example/lixiaohong/beiwanglu/Activity/AddNewNotificationActivity.java
@@ -1,9 +1,22 @@
 package com.example.lixiaohong.beiwanglu.Activity;
 
 import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.lixiaohong.beiwanglu.R;
 
 /**
  * Created by li.xiaohong on 2015/12/22.
  */
 public class AddNewNotificationActivity extends Activity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.add_new_notification_activity);
+    initialResources();
+  }
+
+  public void initialResources(){
+    
+  }
 }
